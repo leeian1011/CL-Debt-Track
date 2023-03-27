02512408e011f2e8e6d1219cebfe08a4e9f6388f
Lee <leeian1011@gmail.com>
2023-03-27 21:44:04 +0800

    added in file_reader function

--- helpfunction/hashtable.c
+++ helpfunction/hashtable.c
@@ -4,54 +4,84 @@
 
 #include "hashtable.h"
 
-void open_file(){
-    FILE *csv = fopen(CSVFILE, "r");
-    if (csv == NULL){
+
+void file_read(FILE* csv){
+    char c;
+    int count = 0;
+    int rows = file_rowcount(csv);
+    int columns = file_columncount(csv);
+
+    while ((c = fgetc(csv)) != EOF){
+        count++;
+        if(c == '\n'){
+            break;
+        }
+    }
+
+    char *buffer = malloc(count);
+    if (buffer == NULL){
         return;
     }
-    //initialize a long variable to store bytes in csv file and set the file pointer back to the start
-    /*long file_size;
-    fseek(csv, 0L, SEEK_END);
-    file_size = ftell(csv);
-    rewind(csv);*/
+    char **date = malloc(sizeof(char*) * rows);
+    if (date == NULL){
+        return;
+    }
 
+    for(int i = 0; i < rows; i++){
+        date[i] = malloc(sizeof(char*) * columns);
+        if (date[i] == NULL){
+            return;
+        }
+    }
+
+    int whileloopcount = 0;
+    while(fgets(buffer, count, csv) != NULL){
+        char *test = strtok(buffer, ",");
+        //strtok returns a POINTER to a char * (i.e the address of buffer)
+        //whenever buffer gets updated to the next line by fgets, strtok's POINTER return value points to the most up to date buffer token
+        //i.e
+        //buffer = "hi, hello"
+        // strtoken("buffer", ",") returns a ->address of hi
+        // if buffer gets set to = "hello, hi"
+        // strtoken("buffer", ",") returns a ->addres of hello.
+        
+        date[whileloopcount] = strdup(test);
+        whileloopcount++;
+    }
+
+    printf("\n%s\n", date[0]);
+    for (int i = 0; i < 4; i++){
+        free(date[i]);
+    }
+    free(buffer);
+}
+
+
+
+int file_rowcount(FILE *csv){
+    int count = 0;
     char c;
-    int cols = 1;
     while ((c = fgetc(csv)) != EOF){
-        if (c == ','){
-            cols++; 
-        }else if (c == '\n'){
-            break;
-        }    
+        if (c == '\n'){
+            count++;
+        }
     }
     rewind(csv);
+    return count;
+}
 
-    int rows = 1;
-    while((c = fgetc(csv)) != EOF){
-        if (c == '\n'){
-            rows++;
+
+int file_columncount(FILE *csv){
+    int count = 0;
+    char c;
+    while ((c = fgetc(csv)) != EOF){
+        if (c == ','){
+            count++;
+        }
+        else if( c == '\n'){
+            rewind(csv);
+            return count;
         }
     }
-    printf("%dcols and %drows\n", cols, rows);
-
-    //char **file_content = malloc();
-    //if(file_content == NULL){
-    //    return;
-    //}
-    
-    //char c;
-    //int temp_count = 0;
-    /*while ((c = (fgetc(csv))) != EOF){
-        if (temp_count > file_size + 1){
-            break;
-        }
-        file_content[temp_count] = c;
-        temp_count++;
-        
-    }*/
-
-    //printf("%c\n", file_content);
-
-    //free(file_content);
-    fclose(csv);
+    return count;
 }
