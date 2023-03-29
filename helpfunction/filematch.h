7129de2494463a2cf16719cff9dd3607de903439
Lee <leeian1011@gmail.com>
2023-03-29 09:33:50 +0800

    Changed hashtable to filematch

---
+++ helpfunction/filematch.h
@@ -0,0 +1,12 @@
+#ifndef FILEMATCH_H
+#define FILEMATCH_H
+
+#define CSVFILE "file/test.csv"
+
+
+void file_read(FILE *csv);
+char *file_parse(char *buffer, FILE *csv, char *date, int file_linesize);
+int file_columncount(FILE *csv);
+int file_rowcount(FILE *csv);
+int regex_match(char* test);
+#endif
