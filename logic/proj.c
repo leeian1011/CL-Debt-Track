413399a049c2b7628a80633d12f9215e82f47c76
Lee <leeian1011@gmail.com>
2023-04-01 20:22:33 +0800

    Added CL UI

--- logic/proj.c
+++ logic/proj.c
@@ -7,10 +7,39 @@
 //function headers
 
 int main(){
-    table_initialize();
     FILE *csv = fopen(CSVFILE, "r");
+    char operations[][10] = {"Add", "Remove", "Remainder", "Find", "Exit"};
+    char query[10];
+
+    table_initialize();
     file_read(csv);
     fclose(csv);
-    table_matchseek("5Jan");
-    table_sum();
+    do{
+        printf("Select an Operation:\n1:Add\n2:Remove\n3:Remainder\n4:Find\n5:Exit\n");
+        fgets(query, 10, stdin);
+        query[strlen(query) - 1]  = '\0';
+
+        if (strcasecmp(query, operations[0]) == 0 || query[0] == '1'){
+            //addfunct();
+            return 0;
+        }
+        else if (strcasecmp(query, operations[1]) == 0 || query[0] == '2'){
+            //removefunct();
+            return 1;
+        }
+        else if (strcasecmp(query, operations[2]) == 0 || query[0] == '3'){
+            //remainderfunct();
+            return 2;
+        }
+        else if (strcasecmp(query, operations[3]) == 0 || query[0] == '4'){
+            //findfunct();
+            return 3;
+        }
+        else if(strcasecmp(query, operations[4]) == 0 || query[0] == '5'){
+            return 0;
+        }
+        else{
+            printf("%s is not an operation!", query);
+        }
+    } while (query[0] == '\0');
 }
